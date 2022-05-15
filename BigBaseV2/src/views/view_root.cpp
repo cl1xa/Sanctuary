35b9dc2b4e081553db14d0e27eb067df109d0d11
Caden <82620517+cl52902@users.noreply.github.com>
2022-05-15 03:07:32 -0400

    Added simple playerlist

--- BigBaseV2/src/views/view_root.cpp
+++ BigBaseV2/src/views/view_root.cpp
@@ -4,6 +4,7 @@
 #include "gta/enums.hpp"
 #include "gta/session_types.hpp"
 #include "util/session.hpp"
+#include "pointers.hpp"
 
 namespace big
 {
@@ -15,7 +16,7 @@ namespace big
 		ImGui::SetNextWindowSize(ImVec2{ 750, 400 }, ImGuiCond_Once);
 		ImGui::SetNextWindowPos(ImVec2{ 100, 200 }, ImGuiCond_Once);
 
-		tabs = tabs = { "Kick", "Crash", "Sync", "Misc", "Session", "Settings"};
+		tabs = tabs = { "Kick", "Crash", "Sync", "Misc", "Session", "Players", "Settings"};
 
 		if (ImGui::Begin("Sanctuary", &g_gui.m_opened, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize))
 		{
@@ -73,7 +74,36 @@ namespace big
 					}
 					break;
 
-				case 5: //Settings
+				case 5:
+					if (*g_pointers->m_is_session_started)
+					{
+						if (ImGui::BeginListBox("##plist", { 580 , 350 })) //Gross hardcoded values, but whatever.
+						{
+							for (auto& item : g_player_service->m_players)
+							{
+								const auto& plyr = item.second;
+
+								if (plyr->is_host())
+									ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.49f, 0.45f, 0.29f, 1.f));
+
+								if (ImGui::Button(plyr->get_name(), { ImGui::GetWindowSize().x - 15.f, 0.f }))
+								{
+									//g_player_service->set_selected(plyr.get());
+								}
+
+								if (plyr->is_host())
+									ImGui::PopStyleColor();
+							}
+
+							ImGui::EndListBox();
+						}
+					}
+					else
+						ImGui::Text("Enter an online session to view the playerlist");
+					
+					break;
+
+				case 6: //Settings
 					ImGui::Checkbox("Notify players", &g_config.settings.notify_players);
 					ImGui::Checkbox("Notify debug", &g_config.settings.notify_debug);
 
